a0f1fb03663c2a6bed00ac62f8300c48ed0e81a7
tomis <chadjiminas.chrysostomos@gmail.com>
2017-11-27 13:54:45 +0200

    query 8a working

--- stored-procedures-queries/Q8_a.sql
+++ stored-procedures-queries/Q8_a.sql
@@ -1,18 +1,24 @@
 IF OBJECT_ID ( 'query8_a', 'P' ) IS NOT NULL
-  DROP PROCEDURE query8_a
+	DROP PROCEDURE query8_a
 GO
 CREATE PROCEDURE query8_a
-AS 
-BEGIN 
-	SELECT 
-	SUM(CASE WHEN P.end_pos = 1 THEN 1 ELSE 0 END) AS countFirstPositions, 
-	SUM(CASE WHEN P.end_pos = 2 THEN 1 ELSE 0 END) AS countSecondPositions, 
-	SUM(CASE WHEN P.end_pos = 3 THEN 1 ELSE 0 END) AS countThirdPositions,
-	COUNT(*) countParticipations,  
-	SUM(CASE WHEN P.end_pos = 1 THEN 1 ELSE 0 END)/COUNT(*) AS successRatio,
-	SUM(P.winnings) AS totalWinnings  
-	FROM PARTICIPATION P,HORSE H
-	WHERE H.id = P.horse_id
-	ORDER BY totalWinnings
-END;
+AS
+	BEGIN
+		SELECT
+			H.id,
+			SUM(CASE WHEN P.end_pos = 1 THEN 1 ELSE 0 END) AS countFirstPositions,
+			SUM(CASE WHEN P.end_pos = 2 THEN 1 ELSE 0 END) AS countSecondPositions,
+			SUM(CASE WHEN P.end_pos = 3 THEN 1 ELSE 0 END) AS countThirdPositions,
+			COUNT(*) countParticipations,
+			SUM(CASE WHEN P.end_pos = 1 THEN 1 ELSE 0 END)/COUNT(*) AS successRatio,
+			SUM(P.winnings) AS totalWinnings
+		FROM PARTICIPATION P,HORSE H
+		WHERE H.id = P.horse_id
+		GROUP BY H.id
+		ORDER BY totalWinnings ASC
+	END;
 
+--EXEC query8_a
+--SELECT *
+--FROM PARTICIPATION P,HORSE H
+--WHERE H.id = P.horse_id
