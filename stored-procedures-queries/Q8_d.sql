6a8d5038eecfa9c407f3229804a4c881328bbb79
tomis <chadjiminas.chrysostomos@gmail.com>
2017-11-28 01:50:20 +0200

    KILL ME.

--- stored-procedures-queries/Q8_d.sql
+++ stored-procedures-queries/Q8_d.sql
@@ -18,6 +18,21 @@ AS
 
     SELECT * FROM #FATHERS_SONS;
 
+    SELECT H.id AS sonId, H.mama_id AS momId,
+           SUM(CASE WHEN P.end_pos = 1 THEN 1 ELSE 0 END) AS countFirstPositions,
+           SUM(CASE WHEN P.end_pos = 2 THEN 1 ELSE 0 END) AS countSecondPositions,
+           SUM(CASE WHEN P.end_pos = 3 THEN 1 ELSE 0 END) AS countThirdPositions,
+           SUM(P.winnings) AS totalWinnings INTO #MOTHERS_SONS
+    FROM HORSE H, PARTICIPATION P
+    WHERE H.id = P.horse_id AND H.mama_id IS NOT NULL AND H.id NOT IN(SELECT FS.sonId FROM #FATHERS_SONS FS)
+    GROUP BY H.id, H.mama_id
+    ORDER BY totalWinnings DESC;
+
+    SELECT * FROM #MOTHERS_SONS;
+
+    --Check wether an eggonos is counted twice. This result-set must be empty.
+    SELECT COUNT(*) FROM #MOTHERS_SONS MS, #FATHERS_SONS FS WHERE MS.sonId=FS.sonId;
+
     SELECT FS.dadId AS father_id, H.dad_id AS grandpa_id,
            SUM(FS.countFirstPositions) AS countFirstPositions,
            SUM(FS.countSecondPositions) AS countSecondPosisions,
@@ -29,17 +44,42 @@ AS
 
     SELECT * FROM #GRANPAS_FATHERS;
 
-    SELECT GF.grandpa_id,
-           SUM(GF.countFirstPositions) AS countFirstPositions,
-           SUM(GF.countSecondPosisions) AS countSecondPosisions,
-           SUM(GF.countThirdPositions) AS countThirdPositions,
-           SUM(GF.totalWinnings) AS totalWinnings INTO #GRANDPAS
-    FROM #GRANPAS_FATHERS GF
-    GROUP BY GF.grandpa_id
+    SELECT MS.momId AS mother_id, H.dad_id AS grandpa_id,
+           SUM(MS.countFirstPositions) AS countFirstPositions,
+           SUM(MS.countSecondPositions) AS countSecondPosisions,
+           SUM(MS.countThirdPositions) AS countThirdPositions,
+           SUM(MS.totalWinnings) AS totalWinnings INTO #GRANPAS_MOTHERS
+    FROM #MOTHERS_SONS MS, HORSE H
+    WHERE H.id = MS.momId AND H.dad_id IS NOT NULL
+    GROUP BY MS.momId, H.dad_id;
 
-    SELECT * FROM #GRANDPAS;
+    SELECT * FROM #GRANPAS_MOTHERS;
+
+    --UNION of GRANPAS MOTHERS AND GRANPAS FATHER TO GET GRANDPAS with PARENTS ids
+    SELECT * INTO #GRANPAS_PARENTS
+    FROM(
+    SELECT GM.mother_id AS parentId, GM.grandpa_id, GM.countFirstPositions, GM.countSecondPosisions, GM.countThirdPositions, GM.totalWinnings FROM #GRANPAS_MOTHERS GM
+    UNION
+    SELECT GF.father_id AS parentId, GF.grandpa_id, GF.countFirstPositions, GF.countSecondPosisions, GF.countFirstPositions, GF.totalWinnings FROM #GRANPAS_FATHERS GF)
+        GF_GM_UNION;
+
+    SELECT * FROM #GRANPAS_PARENTS;
+
+    SELECT GP.grandpa_id,
+           SUM(GP.countFirstPositions) AS countFirstPositions,
+           SUM(GP.countSecondPosisions) AS countSecondPosisions,
+           SUM(GP.countThirdPositions) AS countThirdPositions,
+           SUM(GP.totalWinnings) AS totalWinnings INTO #GRANDPAS
+    FROM #GRANPAS_PARENTS GP
+    GROUP BY GP.grandpa_id
+    ORDER BY totalWinnings DESC
+
+    SELECT * FROM #GRANDPAS ORDER BY totalWinnings DESC;
 
     DROP TABLE #GRANDPAS;
+    DROP TABLE #MOTHERS_SONS;
+    DROP TABLE #GRANPAS_PARENTS;
+    DROP TABLE #GRANPAS_MOTHERS;
     DROP TABLE #GRANPAS_FATHERS;
     DROP TABLE #FATHERS_SONS;
   END
