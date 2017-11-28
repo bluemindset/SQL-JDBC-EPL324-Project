77773429fe17e1b59c6e3b5a2a207d29bf454749
tomis <chadjiminas.chrysostomos@gmail.com>
2017-11-28 23:27:05 +0200

    now query 8a contains horses that won nothing.

--- stored-procedures-queries/Q8_a.sql
+++ stored-procedures-queries/Q8_a.sql
@@ -11,18 +11,32 @@ AS
 			SUM(CASE WHEN P.end_pos = 3 THEN 1 ELSE 0 END) AS countThirdPositions,
 			COUNT(*) countParticipations,
 			ROUND( (CONVERT(FLOAT,SUM(CASE WHEN P.end_pos = 1 THEN 1 ELSE 0 END))/COUNT(*)),5,2) AS successRatio,
-			SUM(P.winnings) AS totalWinnings
+			SUM(P.winnings) AS totalWinnings INTO #TEMP
 		FROM PARTICIPATION P,HORSE H
 		WHERE H.id = P.horse_id
 		GROUP BY H.id, H.name
-		ORDER BY totalWinnings DESC
+		ORDER BY totalWinnings DESC;
+
+-- 		SELECT * FROM #TEMP;
+--     SELECT COUNT(*) FROM #TEMP;
+
+		SELECT T.id, T.name, T.countFirstPositions, T.countSecondPositions, T.countThirdPositions, T.countParticipations, T.successRatio, T.totalWinnings
+		FROM #TEMP T
+		UNION
+		SELECT H.id, H.name, 0 AS countFirstPositions, 0 countSecondPositions, 0 countThirdPositions, 0 AS countParticipations, 0 AS successRatio, 0 AS totalWinnings
+		FROM HORSE H WHERE H.id NOT IN ( SELECT T2.id FROM #TEMP T2)
+
+--     SELECT COUNT(*) FROM
+--       (SELECT T.id, T.name, T.countFirstPositions, T.countSecondPositions, T.countThirdPositions, T.countParticipations, T.successRatio, T.totalWinnings
+--     FROM #TEMP T
+--     UNION
+--     SELECT H.id, H.name, 0 AS countFirstPositions, 0 countSecondPositions, 0 countThirdPositions, 0 AS countParticipations, 0 AS successRatio, 0 AS totalWinnings
+--     FROM HORSE H WHERE H.id NOT IN ( SELECT T2.id FROM #TEMP T2)) TEST
+
+		DROP TABLE #TEMP;
 	END;
 
 --EXEC query8_a
 --SELECT *
 --FROM PARTICIPATION P,HORSE H
 --WHERE H.id = P.horse_id
-
-
-SELECT * 
-FROM MEETING
