86d54789f221402e12217808c7b4c3ad05bd087e
tomis <chadjiminas.chrysostomos@gmail.com>
2017-11-28 23:47:26 +0200

    now query 8c contains jockeys with 0 participation.

--- stored-procedures-queries/Q8_b.sql
+++ stored-procedures-queries/Q8_b.sql
@@ -11,9 +11,34 @@ AS
 			SUM(CASE WHEN P.end_pos = 3 THEN 1 ELSE 0 END) AS countThirdPositions,
 			COUNT(*) countParticipations,
 			ROUND( CONVERT(FLOAT,SUM(CASE WHEN P.end_pos = 1 THEN 1 ELSE 0 END))/COUNT(*) ,5,1) AS successRatio,
-			SUM(P.winnings) AS totalWinnings
+			SUM(P.winnings) AS totalWinnings INTO #TEMP
 		FROM PARTICIPATION P, JOCKEY J
 		WHERE J.id = P.jockey_id
 		GROUP BY J.id, J.first_name, J.last_name, J.compressed_name
-		ORDER BY countFirstPositions DESC
+		ORDER BY countFirstPositions DESC;
+
+-- 		SELECT * FROM #TEMP;
+-- 		SELECT COUNT(*) FROM #TEMP;
+
+		--ADD all Jockeys with no participations.
+	  SELECT T.id, T.first_name, T.last_name, T.compressed_name,
+			T.countFirstPositions, T.countSecondPositions, T.countThirdPositions, T.countParticipations, T.successRatio, T.totalWinnings FROM #TEMP T
+		UNION
+		SELECT J.id, J.first_name, J.last_name, J.compressed_name,
+			0 AS countFirstPositions, 0 AS countSecondPositions, 0 AS countThirdPositions, 0 AS countParticipations, 0 AS successRatio, 0 AS totalWinnings
+		FROM JOCKEY J, PARTICIPATION P WHERE J.id NOT IN ( SELECT T2.id FROM #TEMP T2);
+
+-- SELECT COUNT(*) totalNumberOfJockeys FROM JOCKEY;
+--  //test if all jockeys are added.
+-- 		SELECT COUNT(*) AS numberOfJockeys FROM
+-- 			(SELECT T.id, T.first_name, T.last_name, T.compressed_name,
+-- 		T.countFirstPositions, T.countSecondPositions, T.countThirdPositions, T.countParticipations, T.successRatio, T.totalWinnings FROM #TEMP T
+-- 		UNION
+-- 		SELECT J.id, J.first_name, J.last_name, J.compressed_name,
+-- 			0 AS countFirstPositions, 0 AS countSecondPositions, 0 AS countThirdPositions, 0 AS countParticipations, 0 AS successRatio, 0 AS totalWinnings
+-- 		FROM JOCKEY J WHERE J.id NOT IN ( SELECT T2.id FROM #TEMP T2)) TEST;
+
+		DROP TABLE #TEMP;
 	END;
+
+--EXEC query8_b;
