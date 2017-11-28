c1554d85c3c9721992c2311bc0f5d2672b4256ae
ntzior01 <ntzior01@201ws15.in.cs.ucy.ac.cy>
2017-11-28 18:09:35 +0200

    restored files

---
+++ src/main/java/com/horses/ui/Config.java
@@ -0,0 +1,10 @@
+package com.horses.ui;
+
+public class Config {
+	 	public static final String DATABASE_NAME = "ntzior01";
+	    public static final String DATABASE_SERVER = "apollo.in.cs.ucy.ac.cy:1433";
+	    public static final String DATABASE_USER_ID = "ntzior01";
+	    public static final String DATABASE_PASSWORD = "swuYezukA6RE";
+
+	    public static final String connection_url = "jdbc:sqlserver://apollo.in.cs.ucy.ac.cy:1433;databaseName="+DATABASE_USER_ID+";user="+DATABASE_USER_ID+";password="+DATABASE_PASSWORD+";";
+}
