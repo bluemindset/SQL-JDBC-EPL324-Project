d91e6aeed68694fdab666b86e671e8ff3023d2cc
tomis <chadjiminas.chrysostomos@gmail.com>
2017-11-29 20:14:15 +0200

    Q6 and Q7 done I think.

--- stored-procedures-queries/Q7-selectAllSuccessfulHorses.sql
+++ stored-procedures-queries/Q7-selectAllSuccessfulHorses.sql
@@ -2,13 +2,13 @@ CREATE PROCEDURE selectAllSuccessfulHorses
 AS 
 BEGIN 
 	SET NOCOUNT ON;
-	SELECT *
+	SELECT H.name, H.id
 	FROM HORSE H, PARTICIPATION P
 	WHERE H.id = P.horse_id
-	HAVING (COUNT(*) / (SELECT COUNT(*) AS numOf1stPosParticipations
-					   FROM PARTICIPATION P2
-					   WHERE P2.horse_id = H.id
-					   AND p2.end_pos = '1')) >= (20/100)
+	GROUP BY H.id, H.name
+-- Check if num of 1stPostParticipations != 0.
+	HAVING  (SELECT COUNT(*) AS numOf1stPosParticipations FROM PARTICIPATION P2 WHERE P2.horse_id = H.id AND p2.end_pos = '1') != 0
+						AND
+					(COUNT(*) / (SELECT COUNT(*) AS numOf1stPosParticipations FROM PARTICIPATION P2 WHERE P2.horse_id = H.id AND p2.end_pos = '1')) >= (20/100)
 --(number of all participations / number of 1st position participations) > (20/100)
 END;
---How to compare dates? What is the format for dates ?
