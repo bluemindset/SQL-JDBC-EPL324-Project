fe4f51699b3d870f8515245513257e4e36682d4f
ntzior01 <ntzior01@b121ws15.in.cs.ucy.ac.cy>
2017-11-30 01:21:37 +0200

    Q1

--- src/main/java/com/horses/ui/ViewQ1HorseList.java
+++ src/main/java/com/horses/ui/ViewQ1HorseList.java
@@ -5,16 +5,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.JButton;
 import javax.swing.JTable;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 
 public class ViewQ1HorseList {
 
 	public JFrame frmHorseNameList;
 	private JTextField textField;
-	private JTable table;
+	private JTable tableHorses;
 
 	/**
 	 * Launch the application.
@@ -32,6 +36,115 @@ public class ViewQ1HorseList {
 		});
 	}
 
+	
+	  
+    private void loadRecords(String stringHorse) throws SQLException  {
+    	
+    	String SearchhorseName = stringHorse;
+    	System.out.println(SearchhorseName);
+    	
+        String sql_stmt = "SELECT * FROM [dbo].[HORSE];";
+        
+        ResultSetTableModel tableModel = new ResultSetTableModel(sql_stmt);
+        tableHorses.setModel(tableModel);
+        tableHorses.getSelectionModel().addListSelectionListener((ListSelectionEvent event) -> {
+            try {
+                if (tableHorses.getSelectedRow() >= 0) {
+                	
+                    Object id = tableHorses.getValueAt(tableHorses.getSelectedRow(), 0);
+                    Object name = tableHorses.getValueAt(tableHorses.getSelectedRow(), 1);
+                    Object compressed_name = tableHorses.getValueAt(tableHorses.getSelectedRow(), 2);
+                    Object cur_weight = tableHorses.getValueAt(tableHorses.getSelectedRow(), 3);
+                    Object date_of_birth = tableHorses.getValueAt(tableHorses.getSelectedRow(), 4);
+                    Object age = tableHorses.getValueAt(tableHorses.getSelectedRow(), 5);                    
+                    Object sex = tableHorses.getValueAt(tableHorses.getSelectedRow(), 6);
+                 
+                    Object is_purebred = tableHorses.getValueAt(tableHorses.getSelectedRow(), 7);
+                    Object record = tableHorses.getValueAt(tableHorses.getSelectedRow(), 8);
+                    Object origin_country = tableHorses.getValueAt(tableHorses.getSelectedRow(), 9);
+                    Object mama_id = tableHorses.getValueAt(tableHorses.getSelectedRow(), 10);
+                    Object dad_id = tableHorses.getValueAt(tableHorses.getSelectedRow(), 11); 
+                    Object jockey_id = tableHorses.getValueAt(tableHorses.getSelectedRow(), 12);
+                    Object breeder_id = tableHorses.getValueAt(tableHorses.getSelectedRow(), 13);
+                    Object color_name = tableHorses.getValueAt(tableHorses.getSelectedRow(), 14);
+                    Object trainer_id = tableHorses.getValueAt(tableHorses.getSelectedRow(), 15);
+                    Object owner_id = tableHorses.getValueAt(tableHorses.getSelectedRow(), 16);
+                    
+                    
+//                    
+//                    textFieldID.setText(id.toString());
+//					textFieldName.setText(name.toString());
+//					textFieldWeight.setText(cur_weight.toString());
+//					formattedTextField.setText(date_of_birth.toString());
+//			    	textFieldAge.setText(age.toString());
+//					comboBoxSex.setSelectedItem(sex.toString());
+//					try {
+//						checkBoxIsPurebred.setSelected(Boolean.parseBoolean(is_purebred.toString()));
+//					} catch ( NullPointerException e) {
+//						checkBoxIsPurebred.setSelected(false);
+//					}
+//					try {
+//						comboBoxColor.setSelectedItem(color_name.toString());
+//					} catch ( NullPointerException e) {
+//						comboBoxColor.setSelectedItem(null);
+//					}
+//					try {
+//						comboBoxTrainer.setSelectedItem(trainer_id.toString());
+//					} catch ( NullPointerException e) {
+//						comboBoxTrainer.setSelectedItem(null);
+//					}
+//					try {
+//						comboBoxOwner.setSelectedItem(owner_id.toString());						
+//					} catch ( NullPointerException e) {
+//						comboBoxOwner.setSelectedItem(null);
+//					}
+//					try {
+//						comboBoxBreeder.setSelectedItem(breeder_id.toString());
+//					} catch ( NullPointerException e) {
+//						comboBoxBreeder.setSelectedItem(null);
+//					}
+//					try {						
+//						comboBoxDad.setSelectedItem(dad_id.toString());
+//					} catch ( NullPointerException e) {
+//						comboBoxDad.setSelectedItem(null);
+//					}
+//					try {
+//						comboBoxMom.setSelectedItem(mama_id.toString());						
+//					} catch ( NullPointerException e) {
+//						comboBoxMom.setSelectedItem(null);
+//					}
+//					try {
+//						textFieldOriginCountry.setText(origin_country.toString());
+//					} catch ( NullPointerException e) {
+//						textFieldOriginCountry.setText(null);
+//					}
+//					try {
+//						textFieldRecord.setText(record.toString());
+//					} catch (NullPointerException e) {
+//						textFieldRecord.setText(null);	
+//					} 
+//
+//
+//			    	
+			    	
+
+                    
+                }
+            } catch (Exception ex) {
+            	ex.printStackTrace();
+                System.out.println(ex.getMessage());
+            }
+        });
+        DefaultTableCellRenderer rightRenderer = new DefaultTableCellRenderer();
+        rightRenderer.setHorizontalAlignment(SwingConstants.LEFT);
+        tableHorses.getColumnModel().getColumn(0).setCellRenderer(rightRenderer);
+        tableHorses.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+    }
+
+	
+	
+	
+	
 	/**
 	 * Create the application.
 	 */
@@ -59,6 +172,16 @@ public class ViewQ1HorseList {
 		textField.setColumns(10);
 		
 		JButton btnOk = new JButton("SEARCH");
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					loadRecords(textField.getText());
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnOk.setBounds(218, 40, 89, 23);
 		frmHorseNameList.getContentPane().add(btnOk);
 		
@@ -73,8 +196,10 @@ public class ViewQ1HorseList {
 		btnBack.setBounds(317, 40, 89, 23);
 		frmHorseNameList.getContentPane().add(btnBack);
 		
-		table = new JTable();
-		table.setBounds(10, 98, 406, 338);
-		frmHorseNameList.getContentPane().add(table);
+		tableHorses = new JTable();
+		tableHorses.setBounds(10, 98, 406, 338);
+		frmHorseNameList.getContentPane().add(tableHorses);
+		
+
 	}
 }
