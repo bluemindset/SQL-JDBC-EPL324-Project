7cd81ab155467c0371fd976b8362011bbaf02e38
tomis <chadjiminas.chrysostomos@gmail.com>
2017-11-30 22:21:13 +0200

    fixed some things. Changed config

--- stored-procedures-queries/SQLQuery5.sql
+++ stored-procedures-queries/SQLQuery5.sql
@@ -3,19 +3,15 @@ IF OBJECT_ID('selectTrainerWithNo1winnings', 'P') IS NOT NULL
 GO
 CREATE PROCEDURE selectTrainerWithNo1winnings
 AS 
-BEGIN 
-	
-	
-select TRAINER.id,TRAINER.compressed_name
-FROM TRAINER
-WHERE TRAINER.ID NOT IN (
-		SELECT distinct TRAINER.id 
-		FROM PARTICIPATION
-		LEFT JOIN HORSE ON HORSE.id= PARTICIPATION.star_pos
-		left join TRAINER on PARTICIPATION.trainer_id= TRAINER.id
+BEGIN
+select T.id,T.compressed_name
+FROM TRAINER T
+WHERE T.ID NOT IN (
+		SELECT distinct T.id
+		FROM PARTICIPATION P
+		WHERE P.trainer_id= T.id AND P.end_pos !='1'
 		)
-	
 END;
 
-exec selectTrainerWithNo1winnings
+exec selectTrainerWithNo1winnings;
 
