51385fee2038ab048b2cb856c83a62a1f6e6cc10
achrys16 <achrys16@201ws11.in.cs.ucy.ac.cy>
2017-11-30 12:49:10 +0200

    dfsds

---
+++ stored-procedures-queries/SQLQuery5.sql
@@ -0,0 +1,21 @@
+IF OBJECT_ID('selectTrainerWithNo1winnings', 'P') IS NOT NULL
+    DROP PROCEDURE  selectTrainerWithNo1winnings;
+GO
+CREATE PROCEDURE selectTrainerWithNo1winnings
+AS 
+BEGIN 
+	
+	
+select TRAINER.id,TRAINER.compressed_name
+FROM TRAINER
+WHERE TRAINER.ID NOT IN (
+		SELECT distinct TRAINER.id 
+		FROM PARTICIPATION
+		LEFT JOIN HORSE ON HORSE.id= PARTICIPATION.star_pos
+		left join TRAINER on PARTICIPATION.trainer_id= TRAINER.id
+		)
+	
+END;
+
+exec selectTrainerWithNo1winnings
+
