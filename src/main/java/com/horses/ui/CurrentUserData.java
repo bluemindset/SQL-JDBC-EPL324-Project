2afda1ac1098b2d7a1ac4da22f3be5b74b2b4870
tomis <chadjiminas.chrysostomos@gmail.com>
2017-12-02 22:35:08 +0200

    CurentUserData.

---
+++ src/main/java/com/horses/ui/CurrentUserData.java
@@ -0,0 +1,12 @@
+package com.horses.ui;
+
+/**
+ * Created by tomis on 02/12/2017.
+ */
+public class CurrentUserData {
+    public static String userid;
+
+    public static void setCurUserid (String userid ) {
+        CurrentUserData.userid = userid;
+    }
+}
