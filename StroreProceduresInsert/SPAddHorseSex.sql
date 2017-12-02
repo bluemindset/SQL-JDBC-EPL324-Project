3b96806a308a4b3f6519367335cf9d93dbc85f90
tomis <chadjiminas.chrysostomos@gmail.com>
2017-12-02 18:59:05 +0200

    fixed inserts to drop procedures properly.

--- StroreProceduresInsert/SPAddHorseSex.sql
+++ StroreProceduresInsert/SPAddHorseSex.sql
@@ -1,21 +1,13 @@
-
-Drop PROCEDURE dbo.horseSexInsert
-
+IF OBJECT_ID ( 'horseSexInsert', 'P' ) IS NOT NULL
+  Drop PROCEDURE horseSexInsert
+GO
 CREATE PROCEDURE dbo.horseSexInsert @sex nvarchar(15)
-
-		  
 AS
-
-
-SET NOCOUNT ON
-
+SET NOCOUNT ON;
 
 INSERT INTO [dbo].[HORSE_SEX]
            (   [horse_sex] )
 
      VALUES
            (@sex)
-
 GO
-
-
