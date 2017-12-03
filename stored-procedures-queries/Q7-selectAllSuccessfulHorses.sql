b3c107c5576de996b3910208c023b50829b629c2
tomis <chadjiminas.chrysostomos@gmail.com>
2017-12-03 03:04:25 +0200

    fixed q7. now converts to float and >= 0.2

--- stored-procedures-queries/Q7-selectAllSuccessfulHorses.sql
+++ stored-procedures-queries/Q7-selectAllSuccessfulHorses.sql
@@ -12,8 +12,9 @@ BEGIN
 -- Check if num of 1stPostParticipations != 0.
 	HAVING  (SELECT COUNT(*) AS numOf1stPosParticipations FROM PARTICIPATION P2 WHERE P2.horse_id = H.id AND p2.end_pos = '1') != 0
 						AND
-					((SELECT COUNT(*) AS numOf1stPosParticipations FROM PARTICIPATION P2 WHERE P2.horse_id = H.id AND p2.end_pos = '1')/ COUNT(*)) >= (20/100);
+					(CONVERT(FLOAT, (SELECT COUNT(*) AS numOf1stPosParticipations FROM PARTICIPATION P2 WHERE P2.horse_id = H.id AND p2.end_pos = '1')) / COUNT(*)) >= 0.2;
 --(number of all participations / number of 1st position participations) > (20/100)\
+--CONVERT(FLOAT, (SELECT COUNT(*) AS numOf1stPosParticipations FROM PARTICIPATION P2 WHERE P2.horse_id = 17 AND p2.end_pos = '1'));
 END;
-
+GO
 exec selectAllSuccessfulHorses;
