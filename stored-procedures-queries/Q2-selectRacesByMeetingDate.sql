fbdb8818c71e5cc207b1636aea33bca8ea07f8c6
tomis <chadjiminas.chrysostomos@gmail.com>
2017-12-03 10:56:48 +0200

    fixed query2

--- stored-procedures-queries/Q2-selectRacesByMeetingDate.sql
+++ stored-procedures-queries/Q2-selectRacesByMeetingDate.sql
@@ -6,10 +6,27 @@ CREATE PROCEDURE selectRacesByMeetingDate
 AS
 BEGIN
 	SET NOCOUNT ON;
-	SELECT *
-	FROM RACE R
-	WHERE CONVERT(DATE, R.meeting_date) = CONVERT(DATE, @meetingDate)
+	SELECT P.meeting_date, P.race_time, R.race_type ,P.horse_id,  P.jockey_id, P.trainer_id, P.star_pos, P.end_pos, P.winnings
+	FROM RACE R, PARTICIPATION P
+	WHERE CONVERT(DATE, R.meeting_date) = CONVERT(DATE, @meetingDate) AND
+				(P.meeting_date = R.meeting_date AND P.race_time = R.race_time)
+	ORDER BY meeting_date, race_time, end_pos;
 END;
 GO
-EXEC selectRacesByMeetingDate '2010-01-08';
---How to compare dates? What is the format for dates ?
+-- EXEC selectRacesByMeetingDate '2010-01-08';
+-- EXEC selectRacesByMeetingDate '2010-01-15';
+-- EXEC selectRacesByMeetingDate '2010-01-15';
+-- EXEC selectRacesByMeetingDate '2010-01-22';
+-- EXEC selectRacesByMeetingDate '2010-01-29';
+-- EXEC selectRacesByMeetingDate '2010-02-05';
+-- EXEC selectRacesByMeetingDate '2010-02-12';
+-- EXEC selectRacesByMeetingDate '2010-02-19';
+-- EXEC selectRacesByMeetingDate '2010-02-26';
+-- EXEC selectRacesByMeetingDate '2010-03-05';
+-- EXEC selectRacesByMeetingDate '2010-03-12';
+-- EXEC selectRacesByMeetingDate '2010-03-19';
+-- EXEC selectRacesByMeetingDate '2011-02-04';
+-- EXEC selectRacesByMeetingDate '2011-11-04';
+--
+-- SELECT DISTINCT MEETING.datem from MEETING;
+GO
