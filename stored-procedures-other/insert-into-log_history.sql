36b46df40958472fd49fc492ca9ff463134952b0
tomis <chadjiminas.chrysostomos@gmail.com>
2017-12-03 16:43:35 +0200

    now insert-into-log-history takes currently logged in user instead of USER().

--- stored-procedures-other/insert-into-log_history.sql
+++ stored-procedures-other/insert-into-log_history.sql
@@ -5,10 +5,13 @@ CREATE PROCEDURE insert_into_log_history
 @actionType char(1), @tableName NVARCHAR(25)
 AS
   BEGIN
+    DECLARE @curUserId CHAR(8);
+    EXEC getCurUserId @userId = @curUserId OUTPUT;
     INSERT INTO LOG_HISTORY (action, tmstmp, table_name, user_id)
-    VALUES (@actionType, GETDATE(), @tableName, USER)
+    VALUES (@actionType, GETDATE(), @tableName, @curUserId)
   END
 GO
+--TODO TAKE USER AS A PARAMETER
 -- EXEC insert_into_log_history 'U', 'HORSE';
 -- EXEC insert_into_log_history 'U', 'TRAINER';
 -- SELECT * FROM LOG_HISTORY;
